c61e08eb14d8576db8b9310f9266148b3a0beeab
Jack Herring <jtherring@hotmail.co.uk>
2019-06-04 12:14:19 +0100

    Hardcoding ftw

--- build/buildandrun.ps1
+++ build/buildandrun.ps1
@@ -1,7 +1,3 @@
-param (
-	[string]$password = ""
-)
-
 dotnet restore D:\\teamcity-backup\\src\\TeamCityBackup.sln
 dotnet build D:\\teamcity-backup\\src\\TeamCityBackup.sln -c Release
-D:\\teamcity-backup\\src\\TeamCityBackup\\bin\\Release\\TeamCityBackup.exe /server=http://teamcity.spawtz.com/ /username=autobackup /password=$password /includebuildlogs=false
+D:\\teamcity-backup\\src\\TeamCityBackup\\bin\\Release\\TeamCityBackup.exe /server=http://teamcity.spawtz.com/ /username=autobackup /password=BackTheHellUp33 /includebuildlogs=false
