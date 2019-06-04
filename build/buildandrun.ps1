d6f9047a8ea4473914aff6e7523fa7202a48cc4c
jackherring <jtherring@hotmail.co.uk>
2019-06-04 13:41:19 +0100

    Fix server arg

--- build/buildandrun.ps1
+++ build/buildandrun.ps1
@@ -1,3 +1,3 @@
 dotnet restore D:\\teamcity-backup\\src\\TeamCityBackup.sln
 dotnet build D:\\teamcity-backup\\src\\TeamCityBackup.sln -c Release
-D:\\teamcity-backup\\src\\TeamCityBackup\\bin\\Release\\TeamCityBackup.exe /server=teamcity.spawtz.com/ /username=autobackup /password=BackTheHellUp33 /includebuildlogs=false
+D:\\teamcity-backup\\src\\TeamCityBackup\\bin\\Release\\TeamCityBackup.exe /server=teamcity.spawtz.com /username=autobackup /password=BackTheHellUp33 /includebuildlogs=false
