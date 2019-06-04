aff34f4e783c079b3d45ffcbe21ec77422d32e20
jackherring <jtherring@hotmail.co.uk>
2019-06-04 12:01:04 +0100

    Do it with powershell

---
+++ build/buildandrun.ps1
@@ -0,0 +1,7 @@
+param (
+	[string]$password = ""
+)
+
+dotnet restore D:\\teamcity-backup\\src\\TeamCityBackup.sln
+dotnet build D:\\teamcity-backup\\src\\TeamCityBackup.sln -c Release
+D:\\teamcity-backup\\src\\TeamCityBackup\\bin\\Release\\TeamCityBackup.exe /server=http://teamcity.spawtz.com/ /username=autobackup /password=$password /includebuildlogs=false
